64c31b47166b47e6499b475cb7511b091a2a4377
Miniboy <78801594+Mini3oy@users.noreply.github.com>
2023-09-01 10:50:54 +0700

    first commit

---
+++ src/components/Navber/index.js
@@ -0,0 +1,14 @@
+"use client";
+
+export default function Navber() {
+  return (
+    <>
+      <nav className="bg-white fixed wifull z-20 top-0 left-0 border-b border-grey-200"></nav>
+        <div className="max-w-screen-xl flex flex-wrap items-center justifty-between mx-autu p-4">
+            <div className="flex items-center cursor-pointer">
+                <span className="sle ">ชื่อบริษัท</span>
+            </div>
+        </div>
+    </>
+  );
+}
